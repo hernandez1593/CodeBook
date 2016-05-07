ecd24d257ba371a78e150c1d1b8b250c3543ddd0
yorbigmendez <yorbigmendez@gmail.com>
2016-05-07 11:02:41 -0600

    My Publication .php file edit

--- views/myPublications.php
+++ views/myPublications.php
@@ -1,14 +1,72 @@
-<div class="col-lg-12">
-    <h2>Mis publicaciones</h2>
-
-
-</div>
-
-
-<div class="col-lg-12 myPubs">
-    <div class="col-lg-6">
+<div id="publications" class="col-md-12">
 
 
 
+
+
+
+
+    <div id="publication" class="box2">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Title here</h1>
+                <hr style="hr-style"></hr>
+                <div id="pub-descrip">
+                    <h3>Description here</h3>
+                </div>
+                <hr style="hr-style"></hr>
+                <div id="pub-content">
+                    <h3>Content here</h3>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="col-md-6">
+                    <button id="eliminate-id1">Eliminar</button>
+                </div>
+                <div class="col-md-6">
+                    <button class="changeImgButton" id="modify-1">Modificar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+    <div id="publication" class="box2">
+        <div class="row">
+            <div class="col-md-12" id="publication">
+                <div id="pub-title">
+                    <h1>Title here</h1>
+                    <div id="pub-content">
+                        <h3>Content here</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div id="pub-properties">
+                <div class="col-md-6 col-md-offset-7">
+                    <div class="col-md-3">
+                        <button class="changeImgButton" style="width:100%;" id="eliminate-id1">Eliminar</button>
+                    </div>
+                    <div class="col-md-3">
+                        <button class="changeImgButton" style="width:100%;" id="modify-1">Modificar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-12">
+        <hr style="hr-style">
     </div>
 </div>
